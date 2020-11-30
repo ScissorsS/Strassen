88e21830e49d3aedf18e61886033bb1c127ba5a2
ScissorsS <55027966+ScissorsS@users.noreply.github.com>
2020-11-30 12:04:43 -0800

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/ClearMatrix.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ClearMatrix.dir/main.cpp.o"
+  "ClearMatrix"
+  "ClearMatrix.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ClearMatrix.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
